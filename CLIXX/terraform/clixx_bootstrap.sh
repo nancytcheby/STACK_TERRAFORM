5efb6f82bdbb80cf529b43d35b28280163b2744f
nancytcheby <nancytcheby@hotmail.com>
2025-12-10 13:04:52 -0500

    Clean bootstrap with automated fixes for DB, .htaccess, health.php, and URLs

--- CLIXX/terraform/clixx_bootstrap.sh
+++ CLIXX/terraform/clixx_bootstrap.sh
@@ -133,8 +133,8 @@ cat << 'EOF' | sudo tee /var/www/html/.htaccess
 RewriteEngine On
 RewriteBase /
 RewriteRule ^index\.php$ - [L]
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteCond %{REQUEST_FILENAME} !-d
+RewriteCond %%{REQUEST_FILENAME} !-f
+RewriteCond %%{REQUEST_FILENAME} !-d
 RewriteRule . /index.php [L]
 </IfModule>
 # END WordPress
