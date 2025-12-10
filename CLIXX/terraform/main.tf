bd9a6c98de77d5cb6e07f8312c395c39953bdc7f
nancytcheby <nancytcheby@hotmail.com>
2025-12-09 22:12:22 -0500

    Add wp_config_check_script to bootstrap templatefile

--- CLIXX/terraform/main.tf
+++ CLIXX/terraform/main.tf
@@ -251,7 +251,8 @@ locals {
     db_host                 = aws_db_instance.clixx_db[0].address
     db_name                 = var.clixx_db_name
     db_user                 = var.clixx_db_username
-    db_password_ssm_name    = aws_ssm_parameter.clixx_db_password.name
+    db_pass                 = var.clixx_db_password           
+    db_password_ssm_name    = aws_ssm_parameter.clixx_db_password.name 
     aws_region              = var.aws_region
     domain_name             = var.domain_name
     env                     = var.env
